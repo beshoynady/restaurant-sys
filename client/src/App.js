54da62b3ac4e5a076ef96c402baa90b2c38c0d55
beshoynady <bishoin2017@gmail.com>
2026-02-27 12:24:23 +0200

    App

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 import AppContextProvider from "./context/appContext";
+import { AppContext } from "./context/appContext";
+import LoadingPage from "./layouts/adminLayout/adminComponent/LoadingPage/LoadingPage";
+import NoInternetPage from "./layouts/adminLayout/adminComponent/NoInternetPage/NoInternetPage";
 import AppRoutes from "./routes/appRoutes";
 
 
